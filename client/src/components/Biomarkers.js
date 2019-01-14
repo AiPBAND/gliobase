36499186dd4df31cfa13c7682c465da158c041ec
jeremy <jeremy.geo@gmail.com>
2019-01-14 14:15:13 +0100

    tag colors

--- client/src/components/Biomarkers.js
+++ client/src/components/Biomarkers.js
@@ -2,21 +2,33 @@ import React from 'react';
 import { Query } from "react-apollo";
 import { loader } from 'graphql.macro';
 import Table from 'antd/lib/table';
+import {Tag} from 'antd';
 
 const biomarkersQuery = loader('../queries/biomarkers.graphql');
+ 
+const colors = {
+	DNA: "green",
+	Protein: "cyan",
+	RNA: "magenta"
+}
 
 const columns = [{
 	title: 'ID',
 	dataIndex: 'id',
-	key: 'id',
+	key: 'id'
   }, {
 	title: 'Name',
 	dataIndex: 'name',
-	key: 'name',
+	key: 'name'
   }, {
 	title: 'Category',
 	dataIndex: 'category.name',
 	key: 'category.name',
+	render: cat => (
+		<span>
+		  	<Tag color={colors[cat]} key={cat}>{cat}</Tag>
+		</span>
+	)
   }];
   
 
