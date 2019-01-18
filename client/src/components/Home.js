867b883105f90779b675ba7aea3ff16207073087
Xiaoyu Zhang <zhangxiaoyu11@126.com>
2019-01-18 11:10:51 +0000

    data summary in home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,49 @@
 import React from 'react';
-import { Button, Radio, Icon } from 'antd';
+import { Button, Card, Icon, Avatar, Divider } from 'antd';
 import './Home.css'
+import biomarkerslogo from '../assets/experiment.svg';
+import biomarkersetslogo from '../assets/folder-open.svg';
+import literaturelogo from '../assets/book.svg'
+
+const { Meta } = Card;
+
+const gridStyle = {
+	width: '33%',
+};
 
 const Home = () => {
 	return (
-		<div className = "banner">
-			<div className = "text-wrapper">
-				<h1>
-					Gliobase
-				</h1>
-				<p>
-					A glioblastoma multiforme (GBM) biomarker knowledge base
-				</p>
+		<div className = "content">
+			<div className = "banner">
+				<h1>Gliobase</h1>
+				<p>A glioblastoma multiforme (GBM) biomarker knowledge base</p>
+				<Button icon="github" size="small" href="https://github.com/thehyve/gliobase">Github</Button>
 			</div>
-			<div className = "gitbub-btn">
-				<Button icon="github" size="default" href="https://github.com/thehyve/gliobase">Github</Button>
+			<Divider dashed />
+			<div className = "data-summary">
+				<Card title="Data Summary">
+					<Card.Grid style={gridStyle}>
+						<Meta
+							avatar={<Avatar src={biomarkerslogo} />}
+							title="Biomarkers"
+							description="5"
+						/>
+					</Card.Grid>
+					<Card.Grid style={gridStyle}>
+						<Meta
+							avatar={<Avatar src={biomarkersetslogo} />}
+							title="Biomarker Sets"
+							description="4"
+						/>
+					</Card.Grid>
+					<Card.Grid style={gridStyle}>
+						<Meta
+							avatar={<Avatar src={literaturelogo} />}
+							title="Lierature"
+							description="8"
+						/>
+					</Card.Grid>
+  				</Card>
 			</div>
 		</div>
 	);
