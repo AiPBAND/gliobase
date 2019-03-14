8a51087f1debde78413965869e725373785f190c
Xiaoyu Zhang <zhangxiaoyu11@126.com>
2019-03-14 11:15:51 +0000

    Format change

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,12 +3,12 @@ import { Form, Icon, Input, Button, Checkbox } from 'antd';
 
 class NormalLoginForm extends Component {
     handleSubmit = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
+      e.preventDefault();
+      this.props.form.validateFields((err, values) => {
+          if (!err) {
+              console.log('Received values of form: ', values);
+          }
+      });
     }
   
     render() {
