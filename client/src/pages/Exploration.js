e611ee9c2f56d436fb10cae64436fb2a69bfe4c8
Xiaoyu Zhang <zhangxiaoyu11@126.com>
2019-03-19 13:01:22 +0000

    Get number of every categories for biomarkers

--- client/src/pages/Exploration.js
+++ client/src/pages/Exploration.js
@@ -1,46 +1,54 @@
 import React, {Component} from 'react';
 import { Tabs, Icon } from 'antd';
-import { Pie, yuan } from 'ant-design-pro/lib/Charts';
+import { Pie } from 'ant-design-pro/lib/Charts';
+import { Query } from 'react-apollo';
+import { loader } from 'graphql.macro';
+
+const biomarkersQuery = loader('../queries/biomarkers.graphql');
 
 const TabPane = Tabs.TabPane;
 
 class Exploration extends Component {
 	render(){
-    const salesPieData = [
-      {
-        x: '家用电器',
-        y: 4544,
-      },
-      {
-        x: '食用酒水',
-        y: 3321,
-      },
-      {
-        x: '个护健康',
-        y: 3113,
-      },
-      {
-        x: '服饰箱包',
-        y: 2341,
-      },
-      {
-        x: '母婴产品',
-        y: 1231,
-      },
-      {
-        x: '其他',
-        y: 1231,
-      },
-    ];
+    // const categories = [
+    //   {
+    //     x: 'RNA',
+    //     y: 4544,
+    //   },
+    //   {
+    //     x: 'Aminopeptidase',
+    //     y: 3321,
+    //   },
+    // ];
 
 		return (
       <Tabs type="card">
         <TabPane tab={<span><Icon type="experiment" />Biomarkers</span>} key="1">
-          <Pie
-            className="exploration-pie-chart-biomarker-category"
-            data={salesPieData}
-            height={150}
-          />
+          <Query query={biomarkersQuery}>
+            {({ loading, error, data: biomarkerData }) => {
+              if (loading) return <p>Loading...</p>;
+              if (error) return <p>Error</p>;
+              const biomarkerCategory = biomarkerData.biomarkers.map(el => el.category.id);
+              const tally = biomarkerCategory.reduce((accumulator, el) => {
+                if (accumulator[el] === undefined) {
+                  accumulator[el] = 1;
+                } else {
+                  accumulator[el]++;
+                }
+                return accumulator;
+              }, {});
+              {/* console.log(tally); */}
+              const inputData = Object.entries(tally).map(el => ({ x: el[0], y: el[1]}));
+              {/* console.log(inputData); */}
+              return (
+                <Pie
+                  className="exploration-pie-chart-biomarker-category"
+                  data={inputData}
+                  height={150}
+                />
+              );
+            }}
+          </Query>	
         </TabPane>
         <TabPane tab={<span><Icon type="folder-open" />Biomarker Sets</span>} key="2">
 
