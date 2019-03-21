055998c3e2e5b2ecd4c27ff8fb52d877e8f6370a
Xiaoyu Zhang <zhangxiaoyu11@126.com>
2019-03-21 18:39:41 +0000

    Move all colour setting to a separate file
    In client/src/commons/ColorSettings.js

---
+++ client/src/commons/ColorSettings.js
@@ -0,0 +1,37 @@
+export const CategoryColors = {
+	"DNA": "#003a8c",
+	"RNA": "#1890ff",
+	"Protein": "#13c2c2",
+	"Others": "#006d75"
+}
+
+export const SourceColors = {
+  "Tissue": "#871400",
+	"CSF": "#fa541c",
+	"Blood": "#f5222d",
+	"Serum": "#820014",
+	"Cell line": "#873800",
+	"Cells": "#d46b08",
+	"Others": "#fa8c16"
+}
+
+export const SpeciesColors = {
+	"Homo Sapiens": "#22075e",
+	"Mus Musculus": "#eb2f96",
+	"Macaca mulatta": "#722ed1"
+}
+
+export const StageColors = {
+	"I": "blue",
+	"II": "green",
+	"III": "yellow",
+	"IV": "orange",
+	"V": "red"
+}
+
+export default {
+  CategoryColors,
+  SourceColors,
+  SpeciesColors,
+  StageColors
+};
--- client/src/components/tags/Stage.js
+++ client/src/components/tags/Stage.js
@@ -1,19 +1,12 @@
 import React, {Component} from 'react';
 import { Tag } from 'antd';
+import { StageColors } from '../../commons/ColorSettings';
   
-const colors = {
-	"I": "blue",
-	"II": "green",
-	"III": "yellow",
-	"IV": "orange",
-	"V": "red"
-}
-
 class Stage extends Component {
 	render() {
 		return (
-            <Tag color={colors[this.props.name]}>{this.props.name}</Tag>
-        )
+      <Tag color={StageColors[this.props.name]}>{this.props.name}</Tag>
+    )
 	}
 }
 export default Stage
