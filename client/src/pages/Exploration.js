541ec797eaad97b7be871fedc6b0ff35f2774b5d
Xiaoyu Zhang <zhangxiaoyu11@126.com>
2019-03-22 17:11:27 +0000

    Define a function to shape the data for charts

--- client/src/pages/Exploration.js
+++ client/src/pages/Exploration.js
@@ -16,6 +16,25 @@ const evidencesQuery = loader('../queries/evidencesConcise.graphql');
 
 const TabPane = Tabs.TabPane;
 
+function ShapeDataForCharts(inputData, sortKeys = false) {
+  const tally = inputData.reduce((accumulator, el) => {
+    if (el === null) {
+      ;
+    } else if (accumulator[el] === undefined) {
+      accumulator[el] = 1;
+    } else {
+      accumulator[el]++;
+    }
+    return accumulator;
+  }, {});
+  const keys = Object.keys(tally);
+  if (sortKeys) {
+    keys.sort();
+  }
+  const outputData = keys.map(el => ({ item: el, count: tally[el]}));
+  return outputData
+}
+
 class Exploration extends Component {
 	render(){
     const entityColorSet = ['item', [CategoryColors['DNA'], CategoryColors['Others'], CategoryColors['Protein'], CategoryColors['RNA']]];
@@ -32,20 +51,10 @@ class Exploration extends Component {
               );
               if (error) return <p>Error</p>;
               const entityCategory = entityData.entities.map(el => el.category.id);
-              const tally = entityCategory.reduce((accumulator, el) => {
-                if (accumulator[el] === undefined) {
-                  accumulator[el] = 1;
-                } else {
-                  accumulator[el]++;
-                }
-                return accumulator;
-              }, {});
-              const keys = Object.keys(tally);
-              keys.sort();
-              const inputDataCatagory = keys.map(el => ({ item: el, count: tally[el]}));
+              const entityCategoryChart = ShapeDataForCharts(entityCategory, true);
               return (
                 <div className='exploration-statistics'>
-                  <PieChart data={inputDataCatagory} intervalWidth={2} colorSet={entityColorSet} chartTitle={"Catagory"}/>
+                  <PieChart data={entityCategoryChart} intervalWidth={2} colorSet={entityColorSet} chartTitle={"Catagory"}/>
                   <Entities data={entityData.entities}/>
                 </div>
               );
@@ -77,22 +86,28 @@ class Exploration extends Component {
                 </div>
               );
               if (error) return <p>Error</p>;
-
               const evidenceSource = evidenceData.evidences.map(el => el.sourceIds).flat();
-              console.log(evidenceSource);
-              const tally = evidenceSource.reduce((accumulator, el) => {
-                if (accumulator[el] === undefined) {
+              const evidenceSourceChart = ShapeDataForCharts(evidenceSource, false)
+
+              //Get statistics of region
+              const evidenceRegion = evidenceData.evidences.map(el => el.region).flat();
+              const regionTally = evidenceRegion.reduce((accumulator, el) => {
+                if (el === null) {
+                  ;
+                } else if (accumulator[el] === undefined) {
                   accumulator[el] = 1;
                 } else {
                   accumulator[el]++;
                 }
                 return accumulator;
               }, {});
-              const inputDataSource = Object.entries(tally).map(el => ({ item: el[0], count: el[1]}));
+              const regionKeys = Object.keys(regionTally);
+              regionKeys.sort();
+              const inputEvidenceRegion = regionKeys.map(el => ({ item: el, count: regionTally[el]}));
 
               return (
                 <div className='exploration-statistics'>
-                  <BarChart data={inputDataSource} chartTitle={"Source"}/>
+                  <BarChart data={evidenceSourceChart} chartTitle={"Source"}/>
                   <Evidences data={evidenceData.evidences}/>
                 </div>
               );
