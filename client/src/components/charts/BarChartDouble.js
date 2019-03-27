c30670302a1d57daf70af026514bf43ae3e6ac28
Xiaoyu Zhang <zhangxiaoyu11@126.com>
2019-03-27 20:19:18 +0000

    Add application bar chart to exploration page

--- client/src/components/charts/BarChartDouble.js
+++ client/src/components/charts/BarChartDouble.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Chart, Geom, Axis, Legend, Tooltip } from 'bizcharts'
+import DataSet from "@antv/data-set";
 
 const styles = {
   wrapper: {
@@ -17,28 +18,47 @@ const styles = {
   },
 }
 
+
 class BarChart extends React.Component {
   render () {
+    const data = this.props.data;
+    const ds = new DataSet();
+    const dv = ds.createView().source(data);
+    dv.transform({
+      type: "fold",
+      fields: ["Validated", "Diagnostic", "Predictive", "Prognostic", "Therapeutic"],
+      key: "item",
+      value: "count"
+    });
     return (
       <div style={styles.wrapper}>
         <Chart
           renderer='canvas'
           width={400}
           height={300}
-          data={this.props.data}
+          data={dv}
           padding={[10, 30, 110, 30]}
         >
           <span className='main-title' style={styles.mainTitle}>
             {this.props.chartTitle}
           </span>
-          <Axis name='count'/>
-          <Axis name='item' />
+          <Axis name='item'/>
+          <Axis name='count' />
           <Tooltip
-            showTitle={false}
             itemTpl="<li><span style=&quot;background-color:{color};&quot; class=&quot;g2-tooltip-marker&quot;></span>{name}: {value}</li>"
           />
           <Legend />
-          <Geom type='interval' position='item*count' color='item' />
+          <Geom 
+            type='interval' 
+            position='item*count' 
+            color={'name'} 
+            adjust={[
+              {
+                type: 'dodge',
+                marginRatio: 1 / 32
+              }
+            ]}
+          />
         </Chart>
       </div>
     )
